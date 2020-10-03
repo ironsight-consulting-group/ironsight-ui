e734f3ab123701f76c5c2d2e8179922531891979
Michael Plazek <michael.plazek91@gmail.com>
2020-10-03 13:21:32 -0600

    Add other routes

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { withApp } from "../HOCs"
+
+const About = () => {
+  return (
+    <div>
+      About
+    </div>
+  )
+}
+
+export default withApp(About);
---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { withApp } from "../HOCs"
+
+const Contact = () => {
+  return (
+    <div>
+      Contact
+    </div>
+  )
+}
+
+export default withApp(Contact);
