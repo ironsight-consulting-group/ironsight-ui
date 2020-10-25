af36a1f305af1f6f40eaf60110e1603978202aa8
Michael Plazek <michael.plazek91@gmail.com>
2020-10-25 11:17:16 -0600

    Fix layout of contact form for mobile

--- src/forms/QuestionForm/index.js
+++ src/forms/QuestionForm/index.js
@@ -9,7 +9,6 @@ import { FormikInput } from "../components"
 const QuestionForm = ({ onSubmit }) => {
 	const size = useContext(ResponsiveContext);
 	return (
-		<Box width={size !== 'small' ? 'large' : undefined}>
 			<Formik
 				initialValues={{
 					name: '',
@@ -25,68 +24,79 @@ const QuestionForm = ({ onSubmit }) => {
 				{
 					({ handleChange }) => {
 						return (
-							<Form>
-								<Box direction='row' gap='medium'>
-									<Box gap='small'>
-										<FormikInput
-											name='name'
-											label='Name'
-										/>
-										<FormikInput
-											name='email'
-											label='Email (required)'
-										/>
-										<FormikInput
-											name='phone'
-											label='Phone'
-										/>
-										{
-											size === 'small' && (
-												<Box
-													flex
-													height='small'
-												>
-													<Field
-														as={TextArea}
-														name='question'
-														placeholder='Ask us anything!'
-														onChange={handleChange}
-														fill
-													/>
-												</Box>
-											)
-										}
-										<Button
-											type='submit'
-											label='Submit'
-											primary
-										/>
-									</Box>
-										{
-											size !== 'small' && (
-												<Box
-													flex
-													width={{
-														max: "500px"
-													}}
-												>
-													<Field
-														as={TextArea}
-														name='question'
-														placeholder='Ask us anything!'
-														onChange={handleChange}
-														fill
-													/>
-												</Box>
-											)
-										}
-									</Box>
-							</Form>
+							<Box
+								width={size !== 'small' ? 'large' : '100%'}
+								pad={{ horizontal: 'small' }}
+							>
+								<Form>
+									<Box
+										fill={size !== 'small' ? false : 'horizontal'}
+										direction='row'
+										gap='medium'
+									>
+										<Box
+											fill={size !== 'small' ? false : 'horizontal'}
+											gap='small'
+										>
+											<FormikInput
+												name='name'
+												label='Name'
+											/>
+											<FormikInput
+												name='email'
+												label='Email (required)'
+											/>
+											<FormikInput
+												name='phone'
+												label='Phone'
+											/>
+											{
+												size === 'small' && (
+													<Box
+														flex
+														height='small'
+													>
+														<Field
+															as={TextArea}
+															name='question'
+															placeholder='Ask us anything!'
+															onChange={handleChange}
+															fill
+														/>
+													</Box>
+												)
+											}
+											<Button
+												type='submit'
+												label='Submit'
+												primary
+											/>
+										</Box>
+											{
+												size !== 'small' && (
+													<Box
+														flex
+														width={{
+															max: "500px"
+														}}
+													>
+														<Field
+															as={TextArea}
+															name='question'
+															placeholder='Ask us anything!'
+															onChange={handleChange}
+															fill
+														/>
+													</Box>
+												)
+											}
+										</Box>
+								</Form>
+							</Box>
 						)
 					}
 				}
 			</Formik>
-		</Box>
 	)
 }
 
