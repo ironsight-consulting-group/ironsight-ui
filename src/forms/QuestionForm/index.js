9ab7b57f6e89889e243d1d3e0cd6beffdb9afec2
Michael Plazek <michael.plazek91@gmail.com>
2020-10-25 11:22:57 -0600

    Fix indentation

--- src/forms/QuestionForm/index.js
+++ src/forms/QuestionForm/index.js
@@ -9,94 +9,94 @@ import { FormikInput } from "../components"
 const QuestionForm = ({ onSubmit }) => {
 	const size = useContext(ResponsiveContext);
 	return (
-			<Formik
-				initialValues={{
-					name: '',
-					email: '',
-					phone: '',
-					question: '',
-				}}
-				validationSchema={validationSchema}
-				validateOnBlur={false}
-				validateOnChange={false}
-				onSubmit={onSubmit}
-			>
-				{
-					({ handleChange }) => {
-						return (
-							<Box
-								width={size !== 'small' ? 'large' : '100%'}
-								pad={{ horizontal: 'small' }}
-							>
-								<Form>
+		<Formik
+			initialValues={{
+				name: '',
+				email: '',
+				phone: '',
+				question: '',
+			}}
+			validationSchema={validationSchema}
+			validateOnBlur={false}
+			validateOnChange={false}
+			onSubmit={onSubmit}
+		>
+			{
+				({ handleChange }) => {
+					return (
+						<Box
+							width={size !== 'small' ? 'large' : '100%'}
+							pad={{ horizontal: 'small' }}
+						>
+							<Form>
+								<Box
+									fill={size !== 'small' ? false : 'horizontal'}
+									direction='row'
+									gap='medium'
+								>
 									<Box
 										fill={size !== 'small' ? false : 'horizontal'}
-										direction='row'
-										gap='medium'
+										gap='small'
 									>
-										<Box
-											fill={size !== 'small' ? false : 'horizontal'}
-											gap='small'
-										>
-											<FormikInput
-												name='name'
-												label='Name'
-											/>
-											<FormikInput
-												name='email'
-												label='Email (required)'
-											/>
-											<FormikInput
-												name='phone'
-												label='Phone'
-											/>
-											{
-												size === 'small' && (
-													<Box
-														flex
-														height='small'
-													>
-														<Field
-															as={TextArea}
-															name='question'
-															placeholder='Ask us anything!'
-															onChange={handleChange}
-															fill
-														/>
-													</Box>
-												)
-											}
-											<Button
-												type='submit'
-												label='Submit'
-												primary
-											/>
-										</Box>
-											{
-												size !== 'small' && (
-													<Box
-														flex
-														width={{
-															max: "500px"
-														}}
-													>
-														<Field
-															as={TextArea}
-															name='question'
-															placeholder='Ask us anything!'
-															onChange={handleChange}
-															fill
-														/>
-													</Box>
-												)
-											}
-										</Box>
-								</Form>
-							</Box>
-						)
-					}
+										<FormikInput
+											name='name'
+											label='Name'
+										/>
+										<FormikInput
+											name='email'
+											label='Email (required)'
+										/>
+										<FormikInput
+											name='phone'
+											label='Phone'
+										/>
+										{
+											size === 'small' && (
+												<Box
+													flex
+													height='small'
+												>
+													<Field
+														as={TextArea}
+														name='question'
+														placeholder='Ask us anything!'
+														onChange={handleChange}
+														fill
+													/>
+												</Box>
+											)
+										}
+										<Button
+											type='submit'
+											label='Submit'
+											primary
+										/>
+									</Box>
+										{
+											size !== 'small' && (
+												<Box
+													flex
+													width={{
+														max: "500px"
+													}}
+												>
+													<Field
+														as={TextArea}
+														name='question'
+														placeholder='Ask us anything!'
+														onChange={handleChange}
+														fill
+													/>
+												</Box>
+											)
+										}
+									</Box>
+							</Form>
+						</Box>
+					)
 				}
-			</Formik>
+			}
+		</Formik>
 	)
 }
 
