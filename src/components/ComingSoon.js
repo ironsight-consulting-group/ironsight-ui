42e63af2b69ba5c6fc047f1bc9027fbcfa6cb0ad
Michael Plazek <michael.plazek91@gmail.com>
2020-10-25 18:18:53 -0600

    Add coming soon page

---
+++ src/components/ComingSoon.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { Box, Heading } from "grommet"
+import { UserWorker } from "grommet-icons"
+
+const ComingSoon = () => {
+	return (
+		<Box
+			flex={false}
+			fill={true}
+			align='center'
+			justify='center'
+			direction='row'
+			gap='medium'
+		>
+			<UserWorker color='brand' size='large' />
+			<Heading>Coming soon!</Heading>
+		</Box>
+	)
+}
+
+export default ComingSoon
--- src/pages/services/clinical-trial-recruitment.js
+++ src/pages/services/clinical-trial-recruitment.js
@@ -1,7 +1,8 @@
 import React from "react"
+import ComingSoon from "../../components/ComingSoon"
 
 const ClinicalTrialRecruitment = () => {
-	return <div>Clinical Trial Recruitment</div>
+	return <ComingSoon />
 }
 
 export default ClinicalTrialRecruitment
