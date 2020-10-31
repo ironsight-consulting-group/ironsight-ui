03eb2b8a4a5f4c3d02b9d1edf83abb73d6e8a785
Michael Plazek <michael.plazek91@gmail.com>
2020-10-30 18:51:49 -0600

    Another fix

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -21,6 +21,7 @@ const Page = ({
         width={{ max: '1600px' }}
         fill='horizontal'
         align='start'
+        style={{ overflow: 'hidden' }}
       >
         {children}
       </Box>
