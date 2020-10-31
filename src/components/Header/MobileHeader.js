db22be943b32f10e2ba5443bb511ef793eec9246
Michael Plazek <michael.plazek91@gmail.com>
2020-10-30 20:12:20 -0600

    Try add some other stuff

--- src/components/Header/MobileHeader.js
+++ src/components/Header/MobileHeader.js
@@ -8,6 +8,16 @@ import MobileMenu from "./MobileMenu"
 
 const MobileHeader = ({ links, logo, menus }) => (
   <Fragment>
+    <Box
+      fill='horizontal'
+      direction='row'
+      justify='between'
+      align='center'
+      style={{
+        paddingTop: '100px',
+        marginTop: '-100px',
+      }}
+    >
     <Box margin={{ left: "large" }}>
       <MobileMenu links={links} menus={menus} />
     </Box>
@@ -16,6 +26,7 @@ const MobileHeader = ({ links, logo, menus }) => (
         <Img fluid={logo} alt="Ironsight Logo" />
       </Box>
     </Button>
+    </Box>
   </Fragment>
 )
 
