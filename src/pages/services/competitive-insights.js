12a5605f132f13e9675c5e6f3ace83afa4a3a622
Michael Plazek <michael.plazek91@gmail.com>
2021-01-10 10:57:57 -0700

    Update Competitive Insights page content

--- src/pages/services/competitive-insights.js
+++ src/pages/services/competitive-insights.js
@@ -1,8 +1,52 @@
 import React from "react"
-import ComingSoon from "../../components/ComingSoon"
+import { graphql, useStaticQuery } from "gatsby"
+import { Box } from "grommet"
+import { Contact } from "grommet-icons"
+import Img from "gatsby-image"
+
+import { MobileBasicLink, PageWrapper } from "../../components"
 
 const CompetitiveInsights = () => {
-  return <ComingSoon />
+
+  const data = useStaticQuery(
+    graphql`
+      query CompetitiveInsightsQuery {
+        image: file(relativePath: { eq: "competitive_insights_graph.png" }) {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    `
+  )
+
+  return (
+    <PageWrapper
+      title='Competitive Insights'
+      margin={{ horizontal: 'large', bottom: 'medium' }}
+    >
+      <Box
+        fill='horizontal'
+        gap='medium'
+      >
+        <Img fluid={data.image.childImageSharp.fluid} alt="Stay up to date with current trends"  />
+      </Box>
+      <Box
+        align='center'
+        margin={{ top: 'large' }}
+      >
+        <MobileBasicLink
+          item={{
+            path: '/contact',
+            label: 'Contact us to learn more',
+            icon: <Contact color='blue-3' />
+          }}
+        />
+      </Box>
+    </PageWrapper>
+  )
 }
 
 export default CompetitiveInsights
