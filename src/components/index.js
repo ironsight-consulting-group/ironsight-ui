7b1ba84eb4955c6f0f6a9d91e9783e7c45652001
Michael Plazek <michael.plazek91@gmail.com>
2021-01-10 10:46:18 -0700

    Update Commercialization Support content

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as Page } from './Page';
 export { default as Map } from './Map';
 export { default as BasicLink } from './BasicLink';
 export { default as MobileBasicLink } from './MobileBasicLink';
+export { default as PageWrapper } from './PageWrapper';
--- src/pages/services/commercialization-support.js
+++ src/pages/services/commercialization-support.js
@@ -1,8 +1,52 @@
 import React from "react"
-import ComingSoon from "../../components/ComingSoon"
+import Img from "gatsby-image"
+import { graphql, useStaticQuery } from "gatsby"
+import { Box } from "grommet"
+import { Contact } from "grommet-icons"
+
+import { MobileBasicLink, PageWrapper } from "../../components"
 
 const CommercializationSupport = () => {
-  return <ComingSoon />
+
+  const data = useStaticQuery(
+    graphql`
+      query CommercializationSupportQuery {
+        image: file(relativePath: { eq: "commercialization_support_graph.png" }) {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    `
+  )
+
+  return (
+    <PageWrapper
+      title='Commercialization Support'
+      margin={{ horizontal: 'large', bottom: 'medium' }}
+    >
+      <Box
+        fill='horizontal'
+        gap='medium'
+      >
+        <Img fluid={data.image.childImageSharp.fluid} alt="Providing insightful, flexible solutions"  />
+      </Box>
+      <Box
+        align='center'
+        margin={{ top: 'large' }}
+      >
+        <MobileBasicLink
+          item={{
+            path: '/contact',
+            label: 'Contact us to learn more',
+            icon: <Contact color='blue-3' />
+          }}
+        />
+      </Box>
+    </PageWrapper>
+  )
 }
 
 export default CommercializationSupport
