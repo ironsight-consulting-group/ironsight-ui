6f91f9b04939c4c7932b95e0e1b28ee7f4703999
Michael Plazek <michael.plazek91@gmail.com>
2021-01-10 11:11:38 -0700

    Add Health Informatics Solutions content

--- src/pages/services/health-informatics.js
+++ src/pages/services/health-informatics.js
@@ -1,8 +1,52 @@
 import React from "react"
-import ComingSoon from "../../components/ComingSoon"
+import { graphql, useStaticQuery } from "gatsby"
+import { Box } from "grommet"
+import Img from "gatsby-image"
+import { Contact } from "grommet-icons"
+
+import { MobileBasicLink, PageWrapper } from "../../components"
 
 const HealthInformatics = () => {
-	return <ComingSoon />
+
+	const data = useStaticQuery(
+		graphql`
+        query HealthInformaticsQuery {
+            image: file(relativePath: { eq: "health_informatics_graph.png" }) {
+                childImageSharp {
+                    fluid {
+                        ...GatsbyImageSharpFluid
+                    }
+                }
+            }
+        }
+		`
+	)
+
+	return (
+		<PageWrapper
+			title='Health Informatics Solutions'
+			margin={{ horizontal: 'large', bottom: 'medium' }}
+		>
+			<Box
+				fill='horizontal'
+				gap='medium'
+			>
+				<Img fluid={data.image.childImageSharp.fluid} alt="A patient-centric, data-driven approach to medication management"  />
+			</Box>
+			<Box
+				align='center'
+				margin={{ top: 'large' }}
+			>
+				<MobileBasicLink
+					item={{
+						path: '/contact',
+						label: 'Contact us to learn more',
+						icon: <Contact color='blue-3' />
+					}}
+				/>
+			</Box>
+		</PageWrapper>
+	)
 }
 
 export default HealthInformatics
